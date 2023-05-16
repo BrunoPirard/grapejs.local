3dd197b236288c7d4c60fec4007cc8bd6be923f4
BrunoPirard <bruno.pirard@bulgaweb.com>
2023-05-16 14:19:35 +0300

    16 mai 14

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,47 @@ module.exports = function (eleventyConfig) {
     // Plugins
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
+    // Add Date filters
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+    eleventyConfig.addFilter("date", (dateObj) => {
+        return dayjs(dateObj).format("D MMMM YYYY");
+    });
+
+    eleventyConfig.addFilter("sitemapDate", (dateObj) => {
+        return dayjs(dateObj).toISOString();
+    });
+
+    eleventyConfig.addFilter("year", () => {
+        return dayjs().format("YYYY");
+    });
+
+    /* Markdown plugins */
+    // https://www.11ty.dev/docs/languages/markdown/
+    // --and-- https://github.com/11ty/eleventy-base-blog/blob/master/.eleventy.js
+    // --and-- https://github.com/planetoftheweb/seven/blob/master/.eleventy.js
+    let markdownIt = require("markdown-it")
+    let markdownItFootnote = require("markdown-it-footnote")
+    let markdownItPrism = require('markdown-it-prism')
+    let markdownItBrakSpans = require('markdown-it-bracketed-spans')
+    let markdownItLinkAttrs = require('markdown-it-link-attributes')
+    let markdownItOpts = {
+      html: true,
+      linkify: false,
+      typographer: true
+    }
+    const markdownEngine = markdownIt(markdownItOpts)
+    markdownEngine.use(markdownItFootnote)
+    markdownEngine.use(markdownItPrism)
+    markdownEngine.use(markdownItBrakSpans)
+    markdownEngine.use(markdownItLinkAttrs, {
+      pattern: /^https:/,
+      attrs: {
+        target: '_blank',
+        rel: 'noreferrer noopener'
+      }
+    });
+
     return {
         dir: {
             input: "src",
@@ -23,7 +64,7 @@ module.exports = function (eleventyConfig) {
           '11ty.js'
           ],
         htmlTemplateEngine: "njk",
-        //markdownTemplateEngine: "njk",
+        markdownTemplateEngine: "njk",
         passthroughFileCopy: true,
         
     };
