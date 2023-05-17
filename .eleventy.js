46eab46fbb7fd5a6770fbe2f3ccbda111f1febd0
BrunoPirard <bruno.pirard@bulgaweb.com>
2023-05-17 12:30:53 +0300

    17mai 12

--- .eleventy.js
+++ .eleventy.js
@@ -32,17 +32,35 @@ module.exports = function (eleventyConfig) {
 
     // Add Date filters v2 luxon
     eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
-  });
+        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+    });
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-  });
-
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+    });
+    /* SASS compiler */
     eleventyConfig.setBrowserSyncConfig({
-		  files: './public/assets/css/**/*.css'
+		    files: './public/assets/css/**/*.css'
 	  });
+
+    // Override Browsersync defaults (used only with --serve)
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, browserSync) {
+        const content_404 = fs.readFileSync('_site/404.html');
+
+        browserSync.addMiddleware("*", (req, res) => {
+          // Provides the 404 content without redirect.
+          res.writeHead(404, {"Content-Type": "text/html; charset=UTF-8"});
+          res.write(content_404);
+          res.end();
+        });
+      },
+    },
+    ui: false,
+    ghostMode: false
+  });
 
     /* Markdown plugins */
     // https://www.11ty.dev/docs/languages/markdown/
